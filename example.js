ef6b5bab29a6e1d237eac0177a1bb30ea9ce7df2
James Welford Anderson <james.w.anderson@gmail.com>
2014-07-13 19:37:05 +0900

    initial commit

---
+++ example.js
@@ -0,0 +1,35 @@
+/*\
+title: $:/plugins/welford/twmousetrap/example.js
+type: application/javascript
+module-type: startup
+
+registers a sample keypress thingy
+\*/
+
+(function(){
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Export name and synchronous status
+exports.name = "twmousetrapexample";
+exports.platforms = ["browser"];
+exports.after = ["twmousetrap"];
+exports.synchronous = true;
+
+
+exports.startup = function() {
+	$tw.Mousetrap.bind("ctrl+shift+x",
+		function() {
+			alert("Hi there!");
+		}
+	);
+	$tw.Mousetrap.bind("/",
+		function() {
+			document.getElementById("topsearch").focus();
+		},
+		"keyup"
+	);
+};
+//
+})();
